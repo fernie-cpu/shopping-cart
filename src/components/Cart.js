b4f0c484aa3184f48182a484a9fa63ea73b215b6
fernie-cpu <yoloisjustanick@gmail.com>
2021-03-02 15:46:14 -0300

    render items in cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { ProductContext } from './ProductProvider';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
-  return <div>cart</div>;
+  const value = useContext(ProductContext);
+  const [cart, setCart] = value.cart;
+  const [total, setTotal] = useState(0);
+
+  return (
+    <div>
+      {cart.map((product) => {
+        return (
+          <div className='details' key={product.id}>
+            <div
+              className='img-container'
+              style={{ backgroundImage: `url(${product.img})` }}
+            ></div>
+            <div className='box-details'>
+              <h2>T-SHIRT {product.text}</h2>
+              <p>$ {product.price}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Cart;
