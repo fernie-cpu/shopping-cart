e58902d6766e82b08fb787fcfe169dd3079860a7
fernie-cpu <yoloisjustanick@gmail.com>
2021-03-02 16:15:02 -0300

    move div total and add functionality to get total

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { ProductContext } from './ProductProvider';
 import { Link } from 'react-router-dom';
 
@@ -6,6 +6,20 @@ const Cart = () => {
   const value = useContext(ProductContext);
   const [cart, setCart] = value.cart;
   const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const getTotal = () => {
+      const res = cart.reduce((prev, item) => {
+        return prev + item.price * item.count;
+      }, 0);
+      setTotal(res);
+    };
+    getTotal();
+  }, [cart]);
+
+  if (cart.length === 0) {
+    return <h2>Cart empty</h2>;
+  }
 
   return (
     <div>
@@ -21,23 +35,22 @@ const Cart = () => {
               <p>$ {product.price}</p>
 
               <div className='amount'>
-                <button> - </button>
+                <button className='count'> - </button>
                 <span>{product.count}</span>
-                <button> + </button>
+                <button className='count'> + </button>
               </div>
 
               <div className='delete'>
-                <button>remove from cart</button>
+                <button className='delItem'>remove from cart</button>
               </div>
-            </div>
-
-            <div className='total'>
-              <Link to='/checkout'>Checkout</Link>
-              <h3>Total: $ {total}</h3>
             </div>
           </div>
         );
       })}
+      <div className='total'>
+        <Link to='/checkout'>Checkout</Link>
+        <h3>Total: $ {total}</h3>
+      </div>
     </div>
   );
 };
